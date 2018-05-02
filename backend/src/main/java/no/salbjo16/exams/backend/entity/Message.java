6bfd0e09b9c466a38e0780a1699a28d81aea212d
bjornosal <bjorn.olav.salvesen@gmail.com>
2018-05-02 13:44:26 +0200

    Finished MessageService possibly

--- backend/src/main/java/no/salbjo16/exams/backend/entity/Message.java
+++ backend/src/main/java/no/salbjo16/exams/backend/entity/Message.java
@@ -30,4 +30,27 @@ public class Message {
         this.id = id;
     }
 
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
 }
--- backend/src/main/java/no/salbjo16/exams/backend/service/MessageService.java
+++ backend/src/main/java/no/salbjo16/exams/backend/service/MessageService.java
@@ -1,4 +1,34 @@
 package no.salbjo16.exams.backend.service;
 
+
+import no.salbjo16.exams.backend.entity.Message;
+import no.salbjo16.exams.backend.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
 public class MessageService {
+
+    @Autowired
+    private EntityManager em;
+
+    public Long sendMessage(String text, User sender, User receiver) {
+        //Sender will always exist as it is the logged in user.
+        User recipient  = em.find(User.class, receiver);
+        if(recipient == null) {
+            throw new IllegalArgumentException("User with email " + receiver.getEmail() + " does not exist.");
+        }
+        Message message = new Message();
+        message.setText(text);
+        message.setSender(sender);
+        message.setReceiver(recipient);
+
+        em.persist(message);
+        return message.getId();
+    }
+
 }
