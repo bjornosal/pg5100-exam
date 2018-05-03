5164e17fc0aebe4b26cc76747f01cb36b96551f1
bjornosal <bjorn.olav.salvesen@gmail.com>
2018-05-03 11:30:12 +0200

    Fixed messages.

--- frontend/src/main/java/no/salbjo16/exams/frontend/controller/MessageController.java
+++ frontend/src/main/java/no/salbjo16/exams/frontend/controller/MessageController.java
@@ -1,6 +1,8 @@
 package no.salbjo16.exams.frontend.controller;
 
+import no.salbjo16.exams.backend.entity.Message;
 import no.salbjo16.exams.backend.service.MessageService;
+import no.salbjo16.exams.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.annotation.SessionScope;
 
@@ -15,14 +17,30 @@ public class MessageController {
     private MessageService messageService;
 
     @Autowired
+    private UserService userService;
+
+    @Autowired
     private UserInfoController userInfoController;
 
     private Map<String, String> messageForm = new HashMap<>();
 
 
-    public void sendMessage(String recipientEmail) {
-        messageService.sendMessage(messageForm.get(recipientEmail), userInfoController.getUserName(),recipientEmail);
-        messageForm.put(recipientEmail, "");
+    public String sendMessage(String recipientEmail) {
+        if(messageForm.get(recipientEmail).isEmpty()) {
+            return "/ui/book-detail.jsf?faces-redirect=true&error=true";
+        } else {
+            messageService.sendMessage(messageForm.get(recipientEmail), userInfoController.getUserName(), recipientEmail);
+            messageForm.put(recipientEmail, "");
+            return "/ui/book-detail.jsf?faces-redirect=true&success=true";
+        }
+    }
+
+    public List<Message> getSentMessages() {
+        return userService.getSentMessages(userInfoController.getUserName());
+    }
+
+    public List<Message> getReceivedMessages() {
+        return userService.getReceivedMessages(userInfoController.getUserName());
     }
 
     public Map<String, String> getMessageForm() {
